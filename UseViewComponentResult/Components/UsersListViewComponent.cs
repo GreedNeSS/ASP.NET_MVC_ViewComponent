eba2e81b08e2c45dabf30372ce37f68710683199
GreedNeSS <ruslan199126@gmail.com>
2022-07-20 16:02:15 +0300

    Added ViewComponentResult example.

---
+++ UseViewComponentResult/Components/UsersListViewComponent.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace UseViewComponentResult.Components
+{
+    public class UsersListViewComponent : ViewComponent
+    {
+        List<string> users = new List<string>
+        {
+            "GreedNeSS",
+            "Marcus",
+            "Henry"
+        };
+
+        public IViewComponentResult Invoke()
+        {
+            return View(users);
+        }
+    }
+}
---
+++ UseViewComponentResult/Views/Home/Components/UsersList/Default.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace UseViewComponentResult.Views.Home.Components.UsersList
+{
+    public class DefaultModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
