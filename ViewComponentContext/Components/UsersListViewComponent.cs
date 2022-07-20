36cc1c688d1a6cf60d72bbbc1182e8efa25fe60c
GreedNeSS <ruslan199126@gmail.com>
2022-07-20 23:06:46 +0300

    Added ViewComponentContext example.

---
+++ ViewComponentContext/Components/UsersListViewComponent.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ViewComponentContext.Components
+{
+    public class UsersListViewComponent : ViewComponent
+    {
+        List<string> users = new List<string>
+        {
+            "GreedNeSS", "Marcus", "Henry", "Ruslan", "Kate"
+        };
+
+        public IViewComponentResult Invoke() 
+        {
+            int number = users.Count;
+
+            if (Request.Query.ContainsKey("number"))
+            {
+                int.TryParse(Request.Query["number"], out number);
+            }
+
+            ViewBag.Users = users.Take(number);
+            ViewData["Header"] = $"Количество пользователей: {number}";
+            return View();
+        }
+    }
+}
