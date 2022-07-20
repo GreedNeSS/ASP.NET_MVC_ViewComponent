63e727e8b60087edda269a590950e2c3d47323ef
GreedNeSS <ruslan199126@gmail.com>
2022-07-20 12:46:46 +0300

    Добавьте файлы проекта.

---
+++ Creating/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Creating.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
